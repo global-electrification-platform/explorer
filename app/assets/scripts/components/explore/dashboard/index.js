b655e598060f155ffe2eded5504a0fe05a9ebc43
Ricardo Mestre <ricardo@duplos.org>
2018-11-16 15:56:26 +0000

    Structure and style explore tablist

--- app/assets/scripts/components/explore/dashboard/index.js
+++ app/assets/scripts/components/explore/dashboard/index.js
@@ -7,7 +7,21 @@ import Filters from './Filters';
 class Explore extends Component {
   render () {
     return (
-      <div>
+      <div className='econtrols'>
+        <nav className='nav'>
+          <ul className='nav__tablist' role='tablist'>
+            <li className='nav__tab' role='presentation'>
+              <a href='#econtrols__scenarios' className='nav__link nav__link--active' title='View options' role='tab'><span>Scenarios</span></a>
+            </li>
+            <li className='nav__tab' role='presentation'>
+              <a href='#econtrols__filters' className='nav__link' title='View options' role='tab'><span>Filters</span></a>
+            </li>
+            <li className='nav__tab' role='presentation'>
+              <a href='#econtrols__layers' className='nav__link' title='View options' role='tab'><span>Layers</span></a>
+            </li>
+          </ul>
+        </nav>
+
         <Levers />
         <br />
         <Filters />
