9a7a89a63d60149811ab960e04720b26a32c2b60
Ricardo Mestre <ricardo@duplos.org>
2018-11-26 15:39:33 +0000

    Style and structure country card list. Fix #14 #15 #32

--- app/assets/scripts/views/SelectCountry.js
+++ app/assets/scripts/views/SelectCountry.js
@@ -19,13 +19,75 @@ class SelectCountry extends Component {
             <ol className='country-list card-list'>
               <li className='country-list__item'>
                 <article className='card card--sumary card--country'>
-                  <a className='card__contents'>
+                  <a href='#' className='card__contents' title='Select country'>
+                    <figure className='card__media'>
+                      <div className='card__thumb'>
+                        <img width='640' height='480' src='/assets/graphics/content/flags-4x3/mw.svg' alt='Country flag' />
+                      </div>
+                    </figure>
                     <header className='card__header'>
-                      <h1 className='card__title'>Country name</h1>
+                      <h1 className='card__title'>Malawi</h1>
                     </header>
-                    <div className='card__body'>
-                      <p>Card body.</p>
-                    </div>
+                  </a>
+                </article>
+              </li>
+
+              <li className='country-list__item'>
+                <article className='card card--sumary card--country'>
+                  <a href='#' className='card__contents' title='Select country'>
+                    <figure className='card__media'>
+                      <div className='card__thumb'>
+                        <img width='640' height='480' src='/assets/graphics/content/flags-4x3/mw.svg' alt='Country flag' />
+                      </div>
+                    </figure>
+                    <header className='card__header'>
+                      <h1 className='card__title'>Malawi</h1>
+                    </header>
+                  </a>
+                </article>
+              </li>
+
+              <li className='country-list__item'>
+                <article className='card card--sumary card--country'>
+                  <a href='#' className='card__contents' title='Select country'>
+                    <figure className='card__media'>
+                      <div className='card__thumb'>
+                        <img width='640' height='480' src='/assets/graphics/content/flags-4x3/mw.svg' alt='Country flag' />
+                      </div>
+                    </figure>
+                    <header className='card__header'>
+                      <h1 className='card__title'>Malawi</h1>
+                    </header>
+                  </a>
+                </article>
+              </li>
+
+              <li className='country-list__item'>
+                <article className='card card--sumary card--country'>
+                  <a href='#' className='card__contents' title='Select country'>
+                    <figure className='card__media'>
+                      <div className='card__thumb'>
+                        <img width='640' height='480' src='/assets/graphics/content/flags-4x3/mw.svg' alt='Country flag' />
+                      </div>
+                    </figure>
+                    <header className='card__header'>
+                      <h1 className='card__title'>Malawi</h1>
+                    </header>
+                  </a>
+                </article>
+              </li>
+
+              <li className='country-list__item'>
+                <article className='card card--sumary card--country'>
+                  <a href='#' className='card__contents' title='Select country'>
+                    <figure className='card__media'>
+                      <div className='card__thumb'>
+                        <img width='640' height='480' src='/assets/graphics/content/flags-4x3/mw.svg' alt='Country flag' />
+                      </div>
+                    </figure>
+                    <header className='card__header'>
+                      <h1 className='card__title'>Malawi</h1>
+                    </header>
                   </a>
                 </article>
               </li>
