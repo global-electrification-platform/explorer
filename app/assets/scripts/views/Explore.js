f485d4d6bd82b55aa53ee9aecec6642f3f587859
Daniel da Silva <daniel@developmentseed.org>
2018-12-20 18:29:05 +0000

    Remove layer selection from dashboard

--- app/assets/scripts/views/Explore.js
+++ app/assets/scripts/views/Explore.js
@@ -220,10 +220,8 @@ class Explore extends Component {
                 updateScenario={this.updateScenario}
                 handleLeverChange={this.handleLeverChange}
                 handleFilterChange={this.handleFilterChange}
-                handleLayerChange={this.handleLayerChange}
                 leversState={this.state.leversState}
                 filtersState={this.state.filtersState}
-                layersState={this.state.layersState}
               />
             </header>
             <div className='inpage__body'>
