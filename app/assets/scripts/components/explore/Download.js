e743a9364fb4e98fd281f025eb7f2f12149c9e52
Olaf Veerman <olaf.veerman@gmail.com>
2019-01-03 11:59:42 -0500

    Make the map square and add technology legend

--- app/assets/scripts/components/explore/Download.js
+++ app/assets/scripts/components/explore/Download.js
@@ -44,7 +44,7 @@ function drawFooter (doc, options) {
   doc.fillColor(options.primaryColor)
     .font(options.boldFont)
     .text(config.appTitle, options.margin + 20 + 8, options.pageHeight - (options.margin * 1.5), {
-      width: options.column,
+      width: options.colWidthTwoCol,
       height: 16,
       align: 'left',
       link: config.baseUrl
@@ -54,29 +54,28 @@ function drawFooter (doc, options) {
   doc.fillColor(options.secondaryFontColor)
     .font(options.baseFont)
     .text(config.baseUrl, options.margin + 20 + 8, options.pageHeight - (options.margin * 1.5) + 12, {
-      width: options.column,
+      width: options.colWidthTwoCol,
       height: 16,
       align: 'left'
     });
 
   // Right license
-  doc.text('Creative Commons BY 4.0', options.pageWidth - options.column - options.margin, options.pageHeight - options.margin * 1.5, {
-    width: options.column,
+  doc.text('Creative Commons BY 4.0', options.pageWidth - options.colWidthTwoCol - options.margin, options.pageHeight - options.margin * 1.5, {
+    width: options.colWidthTwoCol,
     height: 16,
     align: 'right',
     link: 'https://creativecommons.org/licenses/by/4.0/'
   });
 
   // Right date
-  doc.text(new Date().getFullYear(), options.pageWidth - options.column - options.margin, options.pageHeight - options.margin * 1.5 + 12, {
-    width: options.column,
+  doc.text(new Date().getFullYear(), options.pageWidth - options.colWidthTwoCol - options.margin, options.pageHeight - options.margin * 1.5 + 12, {
+    width: options.colWidthTwoCol,
     height: 16,
-    align: 'right',
+    align: 'right'
   });
 }
 
 export function downloadPDF (props) {
-  console.log(props)
   const doc = new PDFDocument();
   const stream = doc.pipe(blobStream());
 
@@ -95,19 +94,20 @@ export function downloadPDF (props) {
     baseFontColor: '#3a455c',
     secondaryFontColor: '#6d788f',
     primaryColor: '#5860ff',
-    column: 252,
-    gutter: 28,
+    colWidthTwoCol: 252,
+    gutterTwoCol: 28,
+    colWidthThreeCol: 160,
+    gutterThreeCol: 26,
     margin: 40
   };
 
-  // Limit map height to 344px. Which is roughly 16:9 with a 612 options.pageWidth
-  const mapHeight = options.pageWidth * aspectRatio > 344 ? 344 : options.pageWidth * aspectRatio;
-  const mapWidth = mapHeight / aspectRatio;
-  const mapMargin = mapWidth === options.pageWidth ? 0 : (options.pageWidth - mapWidth) / 2;
+  // Limit map height to a column width. This results in a square aspect ratio of the map
+  const mapWidth = options.colWidthThreeCol * 2 + options.gutterThreeCol;
+  const mapHeight = aspectRatio > 1 ? mapWidth : mapWidth * aspectRatio;
 
   const { country, model, scenario, leversState, filtersState } = props;
 
-  // HEADER
+  // // HEADER
 
   // Left Title
   doc.fillColor(options.baseFontColor)
@@ -122,29 +122,27 @@ export function downloadPDF (props) {
   // Right Title
   doc.fillColor(options.baseFontColor)
     .fontSize(12)
-    .text(config.appTitle, options.pageWidth - options.column - options.margin, options.margin, {
-      width: options.column,
+    .text(config.appTitle, options.pageWidth - options.colWidthTwoCol - options.margin, options.margin, {
+      width: options.colWidthTwoCol,
       align: 'right'
     });
 
   // Right Subtitle
   doc.fillColor(options.secondaryFontColor)
     .fontSize(8)
-    .text(config.appDescription, options.pageWidth - options.column - options.margin, options.margin + 16, {
-      width: options.column,
+    .text(config.appDescription, options.pageWidth - options.colWidthTwoCol - options.margin, options.margin + 16, {
+      width: options.colWidthTwoCol,
       height: 16,
       align: 'right'
     });
 
-  // Map Body Base
-  doc.rect(0, options.headerHeight, options.pageWidth, mapHeight)
-    .fillColor('#E1E1E1')
-    .fill();
+  // // MAP AREA
+  // Map area has a three column layout
 
-  // Map
-  doc.image(dataURL, mapMargin, options.headerHeight, { fit: [options.pageWidth, mapHeight] });
+  // Map (2/3)
+  doc.image(dataURL, options.margin, options.headerHeight, { fit: [options.pageWidth, mapHeight] });
 
-  // Map outline
+  // Map area outline
   doc.rect(0, options.headerHeight, options.pageWidth, 1)
     .fillColor('#192F35', 0.08)
     .fill();
@@ -153,7 +151,38 @@ export function downloadPDF (props) {
     .fillColor('#192F35', 0.08)
     .fill();
 
-  // Secondary Body Base
+  // Legend (1/3)
+  let legendLeft = options.pageWidth - options.margin - options.colWidthThreeCol;
+
+  // Legend header
+  doc.fontSize(12);
+  doc.fillColor(options.baseFontColor, 1)
+    .text('Technologies', legendLeft, options.headerHeight + 20);
+
+  doc.rect(legendLeft, options.headerHeight + 38, 28, 2)
+    .fill(options.primaryColor);
+
+  // Legend
+  const legendKeys = Object.keys(scenario.data.layers);
+  legendKeys.forEach((key, index) => {
+    // Currently picked up from the app config. Will be switched to model config from the props
+    let legendItem = config.techLayers.find(l => l.id === key);
+
+    let itemTop = options.headerHeight + 56 + (index * 24);
+
+    // Legend marker
+    doc.fill(legendItem.color)
+      .roundedRect(legendLeft, itemTop + 1, 12, 4, 2);
+
+    // Legend label
+    doc.fillColor(options.secondaryFontColor, 1)
+      .fontSize(8)
+      .text(prettifyString(legendItem.label), legendLeft + 12 + 4, itemTop);
+  });
+
+  // // BODY
+  // Body has a 2 column layout
+
   doc.rect(0, options.headerHeight + mapHeight, options.pageWidth, options.pageHeight - (options.headerHeight + mapHeight) - options.margin * 2)
     .fill('#f6f7f7');
 
@@ -173,30 +202,30 @@ export function downloadPDF (props) {
   doc.fontSize(8);
   doc.fillColor(options.baseFontColor)
     .text(lorem, options.margin, options.headerHeight + mapHeight + 52, {
-      width: options.column,
+      width: options.colWidthTwoCol,
       align: 'left'
     });
 
   // Result Header
   doc.fontSize(12);
   doc.fillColor(options.baseFontColor)
-    .text('Results', options.margin + options.column + options.gutter, options.headerHeight + mapHeight + 20);
+    .text('Results', options.margin + options.colWidthTwoCol + options.gutterTwoCol, options.headerHeight + mapHeight + 20);
 
-  doc.rect(options.margin + options.column + options.gutter, options.headerHeight + mapHeight + 38, 28, 2)
+  doc.rect(options.margin + options.colWidthTwoCol + options.gutterTwoCol, options.headerHeight + mapHeight + 38, 28, 2)
     .fill(options.primaryColor);
 
   const loremTwo = 'A summary of the results.';
   doc.fontSize(8);
   doc.fillColor(options.baseFontColor)
-    .text(loremTwo, options.margin + options.column + options.gutter, options.headerHeight + mapHeight + 52, {
-      width: options.column,
+    .text(loremTwo, options.margin + options.colWidthTwoCol + options.gutterTwoCol, options.headerHeight + mapHeight + 52, {
+      width: options.colWidthTwoCol,
       align: 'left'
     });
 
   // Levers
   const levers = model.data.levers;
   levers.forEach((lever, index) => {
-    let leverOption = lever.options.find(o => o.id === leversState[index])
+    let leverOption = lever.options.find(o => o.id === leversState[index]);
 
     doc.fontSize(8);
     doc.fillColor(options.secondaryFontColor, 1)
@@ -205,12 +234,12 @@ export function downloadPDF (props) {
     doc.fontSize(8);
     doc.fillColor(options.baseFontColor)
       .text((leverOption.value), options.margin, options.headerHeight + mapHeight + 112 - 2 + (index * 24), {
-        width: options.column,
+        width: options.colWidthTwoCol,
         align: 'right'
       });
 
     if (index !== levers.length - 1) {
-      doc.rect(options.margin, options.headerHeight + mapHeight + 126 + (index * 24), options.column, 1)
+      doc.rect(options.margin, options.headerHeight + mapHeight + 126 + (index * 24), options.colWidthTwoCol, 1)
         .fillColor('#192F35', 0.08)
         .fill();
     }
@@ -226,17 +255,17 @@ export function downloadPDF (props) {
   outputs.forEach((output, index) => {
     doc.fontSize(8);
     doc.fillColor(options.secondaryFontColor, 1)
-      .text(output.name, options.margin + options.column + options.gutter, options.headerHeight + mapHeight + 112 - 2 + (index * 24));
+      .text(output.name, options.margin + options.colWidthTwoCol + options.gutterTwoCol, options.headerHeight + mapHeight + 112 - 2 + (index * 24));
 
     doc.fontSize(8);
     doc.fillColor(options.baseFontColor)
-      .text(output.value, options.pageWidth - options.column - options.margin, options.headerHeight + mapHeight + 112 - 2 + (index * 24), {
-        width: options.column,
+      .text(output.value, options.pageWidth - options.colWidthTwoCol - options.margin, options.headerHeight + mapHeight + 112 - 2 + (index * 24), {
+        width: options.colWidthTwoCol,
         align: 'right'
       });
 
     if (index !== outputs.length - 1) {
-      doc.rect(options.margin + options.column + options.gutter, options.headerHeight + mapHeight + 126 + (index * 24), options.column, 1)
+      doc.rect(options.margin + options.colWidthTwoCol + options.gutterTwoCol, options.headerHeight + mapHeight + 126 + (index * 24), options.colWidthTwoCol, 1)
         .fillColor('#192F35', 0.08)
         .fill();
     }
@@ -258,7 +287,7 @@ export function downloadPDF (props) {
   doc.fontSize(10);
   doc.fillColor(options.secondaryFontColor)
     .text(model.data.description, options.margin, options.headerHeight + 52, {
-      // width: options.column,
+      // width: options.colWidthTwoCol,
       align: 'left'
     });
 
