59df599041ff85df9d1916cb546c441b7e5b1760
Olaf Veerman <olaf.veerman@gmail.com>
2019-01-03 09:48:49 -0500

    Tidy up the header and footer

--- app/assets/scripts/components/explore/Download.js
+++ app/assets/scripts/components/explore/Download.js
@@ -53,17 +53,25 @@ function drawFooter (doc, options) {
   // Left Subtitle
   doc.fillColor(options.secondaryFontColor)
     .font(options.baseFont)
-    .text(config.appDescription, options.margin + 20 + 8, options.pageHeight - (options.margin * 1.5) + 12, {
+    .text(config.baseUrl, options.margin + 20 + 8, options.pageHeight - (options.margin * 1.5) + 12, {
       width: options.column,
       height: 16,
       align: 'left'
     });
 
-  // Right date
-  doc.text('© ' + new Date().getFullYear(), options.pageWidth - options.column - options.margin, options.pageHeight - options.margin * 1.5, {
+  // Right license
+  doc.text('Creative Commons BY 4.0', options.pageWidth - options.column - options.margin, options.pageHeight - options.margin * 1.5, {
     width: options.column,
     height: 16,
-    align: 'right'
+    align: 'right',
+    link: 'https://creativecommons.org/licenses/by/4.0/'
+  });
+
+  // Right date
+  doc.text(new Date().getFullYear(), options.pageWidth - options.column - options.margin, options.pageHeight - options.margin * 1.5 + 12, {
+    width: options.column,
+    height: 16,
+    align: 'right',
   });
 }
 
@@ -81,6 +89,7 @@ export function downloadPDF (props) {
   const options = {
     pageWidth: 612, // Letter; http://www.a4papersize.org/a4-paper-size-in-pixels.php
     pageHeight: 792,
+    headerHeight: 96,
     baseFont: 'Helvetica',
     boldFont: 'Helvetica-Bold',
     baseFontColor: '#3a455c',
@@ -98,38 +107,54 @@ export function downloadPDF (props) {
 
   const { country, model, scenario, leversState, filtersState } = props;
 
-  // Title
-  doc.fontSize(20);
+  // HEADER
+
+  // Left Title
   doc.fillColor(options.baseFontColor)
+    .fontSize(20)
     .text(country.data.name, options.margin, options.margin);
 
+  // Left Subtitle
+  doc.fillColor(options.secondaryFontColor)
+    .fontSize(8)
+    .text(model.data.name, options.margin, options.margin + 24);
+
   // Right Title
-  doc.fontSize(12);
   doc.fillColor(options.baseFontColor)
+    .fontSize(12)
     .text(config.appTitle, options.pageWidth - options.column - options.margin, options.margin, {
       width: options.column,
       align: 'right'
     });
 
+  // Right Subtitle
+  doc.fillColor(options.secondaryFontColor)
+    .fontSize(8)
+    .text(config.appDescription, options.pageWidth - options.column - options.margin, options.margin + 16, {
+      width: options.column,
+      height: 16,
+      align: 'right'
+    });
+
   // Map Body Base
-  doc.rect(0, 80, options.pageWidth, mapHeight)
+  doc.rect(0, options.headerHeight, options.pageWidth, mapHeight)
     .fillColor('#E1E1E1')
     .fill();
 
   // Map
-  doc.image(dataURL, mapMargin, 80, { fit: [options.pageWidth, mapHeight] });
+  doc.image(dataURL, mapMargin, options.headerHeight, { fit: [options.pageWidth, mapHeight] });
 
   // Map outline
-  doc.rect(0, 80, options.pageWidth, 1)
+  doc.rect(0, options.headerHeight, options.pageWidth, 1)
     .fillColor('#192F35', 0.08)
     .fill();
 
-  doc.rect(0, 80 + mapHeight - 1, options.pageWidth, 1)
+  doc.rect(0, options.headerHeight + mapHeight - 1, options.pageWidth, 1)
     .fillColor('#192F35', 0.08)
     .fill();
 
   // Secondary Body Base
-  doc.rect(0, 80 + mapHeight, options.pageWidth, options.pageHeight - (80 + mapHeight) - options.margin * 2)
+  doc.rect(0, options.headerHeight + mapHeight, options.pageWidth, options.pageHeight - (options.headerHeight + mapHeight) - options.margin * 2)
     .fill('#f6f7f7');
 
   doc.rect(0, options.pageHeight - options.margin * 2 - 1, options.pageWidth, 1)
@@ -139,15 +164,15 @@ export function downloadPDF (props) {
   // Indicator Headers
   doc.fontSize(12);
   doc.fillColor(options.baseFontColor, 1)
-    .text('Scenarios', options.margin, 80 + mapHeight + 20);
+    .text('Scenarios', options.margin, options.headerHeight + mapHeight + 20);
 
-  doc.rect(options.margin, 80 + mapHeight + 38, 28, 2)
+  doc.rect(options.margin, options.headerHeight + mapHeight + 38, 28, 2)
     .fill(options.primaryColor);
 
   const lorem = 'These results were generated based on the following assumptions.';
   doc.fontSize(8);
   doc.fillColor(options.baseFontColor)
-    .text(lorem, options.margin, 80 + mapHeight + 52, {
+    .text(lorem, options.margin, options.headerHeight + mapHeight + 52, {
       width: options.column,
       align: 'left'
     });
@@ -155,15 +180,15 @@ export function downloadPDF (props) {
   // Result Header
   doc.fontSize(12);
   doc.fillColor(options.baseFontColor)
-    .text('Results', options.margin + options.column + options.gutter, 80 + mapHeight + 20);
+    .text('Results', options.margin + options.column + options.gutter, options.headerHeight + mapHeight + 20);
 
-  doc.rect(options.margin + options.column + options.gutter, 80 + mapHeight + 38, 28, 2)
+  doc.rect(options.margin + options.column + options.gutter, options.headerHeight + mapHeight + 38, 28, 2)
     .fill(options.primaryColor);
 
   const loremTwo = 'A summary of the results.';
   doc.fontSize(8);
   doc.fillColor(options.baseFontColor)
-    .text(loremTwo, options.margin + options.column + options.gutter, 80 + mapHeight + 52, {
+    .text(loremTwo, options.margin + options.column + options.gutter, options.headerHeight + mapHeight + 52, {
       width: options.column,
       align: 'left'
     });
@@ -175,17 +200,17 @@ export function downloadPDF (props) {
 
     doc.fontSize(8);
     doc.fillColor(options.secondaryFontColor, 1)
-      .text(prettifyString(lever.label), options.margin, 80 + mapHeight + 112 - 2 + (index * 24));
+      .text(prettifyString(lever.label), options.margin, options.headerHeight + mapHeight + 112 - 2 + (index * 24));
 
     doc.fontSize(8);
     doc.fillColor(options.baseFontColor)
-      .text((leverOption.value), options.margin, 80 + mapHeight + 112 - 2 + (index * 24), {
+      .text((leverOption.value), options.margin, options.headerHeight + mapHeight + 112 - 2 + (index * 24), {
         width: options.column,
         align: 'right'
       });
 
     if (index !== levers.length - 1) {
-      doc.rect(options.margin, 80 + mapHeight + 126 + (index * 24), options.column, 1)
+      doc.rect(options.margin, options.headerHeight + mapHeight + 126 + (index * 24), options.column, 1)
         .fillColor('#192F35', 0.08)
         .fill();
     }
@@ -201,17 +226,17 @@ export function downloadPDF (props) {
   outputs.forEach((output, index) => {
     doc.fontSize(8);
     doc.fillColor(options.secondaryFontColor, 1)
-      .text(output.name, options.margin + options.column + options.gutter, 80 + mapHeight + 112 - 2 + (index * 24));
+      .text(output.name, options.margin + options.column + options.gutter, options.headerHeight + mapHeight + 112 - 2 + (index * 24));
 
     doc.fontSize(8);
     doc.fillColor(options.baseFontColor)
-      .text(output.value, options.pageWidth - options.column - options.margin, 80 + mapHeight + 112 - 2 + (index * 24), {
+      .text(output.value, options.pageWidth - options.column - options.margin, options.headerHeight + mapHeight + 112 - 2 + (index * 24), {
         width: options.column,
         align: 'right'
       });
 
     if (index !== outputs.length - 1) {
-      doc.rect(options.margin + options.column + options.gutter, 80 + mapHeight + 126 + (index * 24), options.column, 1)
+      doc.rect(options.margin + options.column + options.gutter, options.headerHeight + mapHeight + 126 + (index * 24), options.column, 1)
         .fillColor('#192F35', 0.08)
         .fill();
     }
@@ -225,14 +250,14 @@ export function downloadPDF (props) {
   // About the model
   doc.fontSize(12);
   doc.fillColor(options.baseFontColor, 1)
-    .text('About the model', options.margin, 80 + 20);
+    .text('About the model', options.margin, options.headerHeight + 20);
 
-  doc.rect(options.margin, 80 + 38, 28, 2)
+  doc.rect(options.margin, options.headerHeight + 38, 28, 2)
     .fill(options.primaryColor);
 
   doc.fontSize(10);
   doc.fillColor(options.secondaryFontColor)
-    .text(model.data.description, options.margin, 80 + 52, {
+    .text(model.data.description, options.margin, options.headerHeight + 52, {
       // width: options.column,
       align: 'left'
     });
