a60f458d6a449c395d4125ed4f89ce7225cc43a4
Vitor George <vitor.george@gmail.com>
2019-09-23 11:35:16 +0100

    Set site and API URL for different environments.

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -3,6 +3,7 @@
 export default {
   environment: 'production',
   // Change in index.html as well.
-  siteUrl: 'http://gep-explorer.surge.sh',
-  dataServiceUrl: 'http://gep-data-service.us-west-2.elasticbeanstalk.com'
+  siteUrl: 'https://electrifynow.info',
+  dataServiceUrl: 'https://gep-api-prod.us-west-2.elasticbeanstalk.com'
+
 };
--- app/assets/scripts/config/staging.js
+++ app/assets/scripts/config/staging.js
@@ -1,5 +1,7 @@
 'use strict';
 
 export default {
-  environment: 'staging'
+  environment: 'staging',
+  siteUrl: 'http://gep-explorer.surge.sh',
+  dataServiceUrl: 'http://gep-data-service.us-west-2.elasticbeanstalk.com'
 };
