e2f5c0db7cc837fb917f74de0bb5d2754a00b63e
Vitor George <vitor.george@gmail.com>
2019-09-23 12:06:38 +0100

    Change API address for production.

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -4,6 +4,5 @@ export default {
   environment: 'production',
   // Change in index.html as well.
   siteUrl: 'https://electrifynow.info',
-  dataServiceUrl: 'https://gep-api-prod.us-west-2.elasticbeanstalk.com'
-
+  dataServiceUrl: 'https://api.electrifynow.info'
 };
