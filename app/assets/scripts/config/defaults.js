52ac5b5429e1f88ebe82962a9b91f34ed1f6c27c
Vitor George <vitor.george@gmail.com>
2020-03-25 15:17:11 +0000

    Add Derilinx's Mapbox key to default config

--- app/assets/scripts/config/defaults.js
+++ app/assets/scripts/config/defaults.js
@@ -7,6 +7,6 @@ export default {
     'Explore least cost electrification strategies around the world.',
   baseUrl: 'http://localhost:9000',
   dataServiceUrl: 'http://localhost:3000',
-  mapboxAccessToken: null,
+  mapboxAccessToken: 'pk.eyJ1IjoiZGVyaWxpbngiLCJhIjoiY2szeTlzbWo2MDV6eDNlcDMxM3dzZXBieiJ9.zPf1iiFilYYwyx6ETNj_8w',
   basemapStyleLink: 'mapbox://styles/derilinx/ck3yqjovg4dpn1crwajrc9ajr'
 };
